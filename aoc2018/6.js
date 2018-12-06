edb9185faf01e3335b07b508dd2e1c2f4517b3d9
Nejc Ramovs <n.ramovs@sportradar.com>
2018-12-06 14:26:32 +0100

    2018 6

--- aoc2018/6.js
+++ aoc2018/6.js
@@ -17,83 +17,73 @@ const testInput = `
 .trim();
 
 
-function run(input) {
+function run(input, limit) {
     let part1 = undefined;
     let part2 = undefined;
     const lines = input.split('\n').filter(line => line.length);
 
-    const coords = [];
     const map = new Grid((x, y) => ({x, y, value: 0}));
+
+    console.time('part1');
+    const data = [];
+    let minx = Number.MAX_SAFE_INTEGER, miny = Number.MAX_SAFE_INTEGER, maxx = 0, maxy = 0;
     lines.forEach((line, ix) => {
         const [x, y] = line.split(', ').map(x => +x);
-        let q = new Queue();
-        coords.push([x, y, q, ix]);
-        // map.get(x, y).id = ix;
-        map.get(x, y).id = String.fromCharCode('A'.codePointAt(0) + ix);
-
-
-        q.enq(map.get(x, y));
+        data.push([x, y, ix]);
+        minx = Math.min(minx, x);
+        miny = Math.min(miny, y);
+        maxx = Math.max(maxx, x);
+        maxy = Math.max(maxy, y);
     });
 
-    const { minCol, maxCol, minRow, maxRow } = map;
-    // map.print(x => x.id === undefined || x.id === -1 ? '.' : x.id);
-
-    const width = maxCol - minCol;
-    const height = maxRow - minRow;
-
-    const sum = {};
-
-    let any = true;
-    let v = 0;
-    while (any) {
-        any = false;
-        for (const data of coords) {
-            const [x, y, queue] = data;
-            // debugger;
-            while(queue.peek() && queue.peek().value === v) {
-                let point = queue.deq();
-                any = true;
-                for (const [dx, dy] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
-                    const x = point.x + dx;
-                    const y = point.y + dy;
-                    // debugger;
-                    if (x >= minCol - width && x <= maxCol + width && y >= minRow - height && y <= maxRow + height) {
-                        const newPoint = map.get(x, y);
-                        if (newPoint.id === -1) {
-
-                        } else if (newPoint.value === v + 1 && newPoint.id !== point.id) {
-                            sum[newPoint.id]--;
-                            newPoint.id = -1;
-                        } else if (newPoint.id === undefined) {
-                            if (sum[point.id] === undefined || sum[point.id] >= 0) {
-                                sum[point.id] = (sum[point.id] || 0) + 1;
-                            }
-
-                            newPoint.id = point.id;
-                            newPoint.value = point.value + 1;
-                            queue.enq(newPoint);
-                        }
-                    } else {
-                        sum[point.id] = -1;
-                    }
+    const areas = {};
+    for (let x = minx; x <= maxx; x++) {
+        for (let y = miny; y <= maxy; y++) {
+            let minDistance = Number.MAX_SAFE_INTEGER;
+            let value = -1;
+            for (const [xx, yy, ix] of data) {
+                const d = Math.abs(xx - x) + Math.abs(yy - y);
+                if (d < minDistance) {
+                    minDistance = d;
+                    value = ix;
+                } else if (d === minDistance) {
+                    value = -1;
+                }
+            }
+            if (value !== -1) {
+                if (x === minx || x === maxx || y === miny || y === maxy) {
+                    areas[value] = Infinity;
+                } else if (areas[value] !== Infinity) {
+                    areas[value] = (areas[value] || 0) + 1;
                 }
             }
         }
-        // map.print(x => x.id === undefined || x.id === -1 ? '.' : x.id);
-        v++;
-        // debugger;
     }
-    // map.print(x => x.id === undefined || x.id === -1 ? '.' : x.id);
 
-    part1 = Math.max(...Object.values(sum)) + 1;
+    part1 = Math.max(...Object.values(areas).filter(x => x < Infinity));
+    console.timeEnd('part1');
 
-    // console.log('foo');
+
+    console.time('part2');
+    part2 = 0;
+    for (let x = minx; x <= maxx; x++) {
+        for (let y = miny; y <= maxy; y++) {
+            let sum = 0;
+            for (const [xx, yy] of data) {
+                sum += Math.abs(xx - x) + Math.abs(yy - y);
+            }
+            if (sum < limit) {
+                part2 += 1;
+            }
+        }
+    }
+    console.timeEnd('part2')
+
     return [part1, part2];
 }
 
-const testResult = run(testInput);
+const testResult = run(testInput, 32);
 console.log('test: ', testResult.join(' / '));
 
-const result = run(input);
+const result = run(input, 10000);
 console.log('result: ', result.join(' / '));
-// high 4829
