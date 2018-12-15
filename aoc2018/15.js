846e5cb50a91d07fcdd24dbacaa68bfcee257a5a
Nejc Ramovš <nejc.ramovs@gmail.com>
2018-12-15 23:18:06 +0100

    2018 15 more optimizations

--- aoc2018/15.js
+++ aoc2018/15.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const Grid = require('../utils/Grid');
+const Grid = require('../utils/FixedGrid');
 const List = require('../utils/LinkedList');
 
 let input = '';
@@ -53,6 +53,17 @@ const testInput = [`
 
 const order = [[0, -1], [-1, 0], [1, 0], [0, 1]];
 
+function run(input) {
+    const part1 = simulate(input, 3);
+
+    let part2;
+    for (let power = 4; !part2; power++) {
+        part2 = simulate(input, power);
+    }
+
+    return [part1, part2];
+}
+
 function simulate(input, attackPower) {
     let { map, players } = createWorld(input);
 
@@ -66,66 +77,25 @@ function simulate(input, attackPower) {
             let enemy = getWeakestEnemy(player, map);
 
             if (!enemy) {
-                let allEnemies = players.filter(x => x.type !== player.type && x.hp > 0);
-                if (allEnemies.length === 0) {
+                let enemies = players.filter(x => x.type !== player.type && x.hp > 0);
+                if (enemies.length === 0) {
+                    // End simulations
                     finished = true;
                     break;
                 }
 
-                const targets = new Set();
-                for (const enemy of allEnemies) {
-                    for (const [dx, dy] of order) {
-                        const loc = map.get(enemy.x + dx, enemy.y + dy);
-                        if (!loc.wall && !loc.player) {
-                            targets.add(map.get(enemy.x + dx, enemy.y + dy));
-                        }
-                    }
+                const targets = getTargets(enemies, map);
+                if (targets.size === 0) {
+                    continue; // Don't run search algorithm if no targets
                 }
-                if (targets.size === 0) continue; // Don't run search algorithm if no targets
 
                 const playerLoc = map.get(player.x, player.y);
-                playerLoc.steps = 0;
-                playerLoc.from = null;
-
-                const list = new List();
-                list.push(playerLoc);
-
-                const visited = new Set([playerLoc])
-
-                let bestTarget = null;
-                while (list._size > 0) {
-                    const curr = list.shift();
-                    if (bestTarget && curr.steps > bestTarget.steps) {
-                        break;
-                    }
-
-                    if (targets.has(curr)) {
-                        if (!bestTarget || curr.y < bestTarget.y || curr.y === bestTarget.y && curr.x < bestTarget.x) {
-                            bestTarget = curr;
-                        }
-                    }
-
-                    for (const [dx, dy] of order) {
-                        const next = map.get(curr.x + dx, curr.y + dy);
-                        if (!next.wall && !next.player && !visited.has(next)) {
-                            next.steps = curr.steps + 1;
-                            next.from = curr;
-                            visited.add(next);
-                            list.push(next);
-                        }
-                    }
-                }
-
-                if (bestTarget) {
-                    // Trace back to first neighbour
-                    while (bestTarget.from.from) {
-                        bestTarget = bestTarget.from;
-                    }
-
-                    map.get(player.x, player.y).player = null;
-                    map.get(bestTarget.x, bestTarget.y).player = player;
-                    player.x = bestTarget.x;
-                    player.y = bestTarget.y;
+                let nextLocation = getNextLocation(playerLoc, targets, map);
+                if (nextLocation) {
+                    playerLoc.player = null;
+                    nextLocation.player = player;
+                    player.x = nextLocation.x;
+                    player.y = nextLocation.y;
 
                     enemy = getWeakestEnemy(player, map);
                 }
@@ -159,10 +129,12 @@ function createWorld(input) {
     const lines = input.split('\n').filter(line => line.length);
 
     let players = [];
-    const map = new Grid((x, y) => ({
+    const map = new Grid(lines[0].length, lines.length, (x, y) => ({
             x, y,
             wall: false,
-            player: null
+            player: null,
+            steps: 0,
+            from: null
     }));
 
     lines.forEach((line, y) => {
@@ -197,6 +169,62 @@ function getWeakestEnemy(player, map) {
     return enemy;
 }
 
+function getTargets(enemies, map) {
+    const targets = new Set();
+    for (const enemy of enemies) {
+        for (const [dx, dy] of order) {
+            const loc = map.get(enemy.x + dx, enemy.y + dy);
+            if (!loc.wall && !loc.player) {
+                targets.add(loc);
+            }
+        }
+    }
+    return targets;
+}
+
+function getNextLocation(playerLoc, targets, map) {
+    playerLoc.steps = 0;
+    playerLoc.from = null;
+
+    const list = new List();
+    list.push(playerLoc);
+
+    const visited = new Set([playerLoc])
+
+    let bestTarget = null;
+    while (list._size > 0) {
+        const curr = list.shift();
+        if (bestTarget && curr.steps > bestTarget.steps) {
+            break;
+        }
+
+        if (targets.has(curr)) {
+            if (!bestTarget || curr.y < bestTarget.y || curr.y === bestTarget.y && curr.x < bestTarget.x) {
+                bestTarget = curr;
+            }
+        }
+
+        for (let i = 0; i < order.length; i++) {
+            const next = map.get(curr.x + order[i][0], curr.y + order[i][1]);
+            if (!next.wall && !next.player && !visited.has(next)) {
+                next.steps = curr.steps + 1;
+                next.from = curr;
+                visited.add(next);
+                list.push(next);
+            }
+        }
+    }
+
+    // Trace back to first neighbour
+    if (bestTarget) {
+        while (bestTarget.from.from) {
+            bestTarget = bestTarget.from;
+        }
+    }
+
+    return bestTarget;
+}
+
 function print(map) {
     map.print(x => {
         if (x.player) {
@@ -207,17 +235,6 @@ function print(map) {
             return '.';
         }
     });
-}
-
-function run(input) {
-    const part1 = simulate(input, 3);
-
-    let part2;
-    for (let power = 4; !part2; power++) {
-        part2 = simulate(input, power);
-    }
-
-    return [part1, part2];
 }
 
 testInput.forEach(input => {
