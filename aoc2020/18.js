594ba8f2d98432ba9b85675f2ce71f780bbd7371
Nejc Ramovs <n.ramovs@sportradar.com>
2020-12-18 23:05:03 +0100

    minor update

--- aoc2020/18.js
+++ aoc2020/18.js
@@ -1,25 +1,88 @@
 const fs = require('fs');
-const sscanf = require('scan.js').scan;
-// @ts-ignore
-const Grid = require('../utils/Grid');
-// @ts-ignore
-const List = require('../utils/LinkedList');
 
 let input = '';
 try {
     input = fs.readFileSync(__filename.replace('.js', '.txt'), 'utf8').trim();
 } catch (e) {}
 const testInputs = [
+    /*
+    1 + (2 * 3) + (4 * (5 + 6))
+    5 + (8 * 3 + 9 + 3 * 4 * 3)
+    5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+    ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
+    */
     `
-1 + (2 * 3) + (4 * (5 + 6))
-2 * 3 + (4 * 5)
-5 + (8 * 3 + 9 + 3 * 4 * 3)
-5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
-((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
+   2 * 3 + (4 * 5)
+
 `
 ].map((x) => x.trim());
 
-// const neighbours = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+function calc1(expr) {
+    const stack = [];
+
+    for (const token of expr) {
+        let result = 0;
+        if (token === ')') {
+            result = stack.pop();
+            stack.pop(); // get rid of (
+        } else if (typeof token === 'number') {
+            result = token;
+        }
+
+        if (result > 0) {
+            if (stack.length > 0 && stack[stack.length - 1] !== '(') {
+                const op = stack.pop();
+                const value = stack.pop();
+                result = operation(value, op, result);
+            }
+            stack.push(result);
+        } else {
+            stack.push(token);
+        }
+    }
+
+    return stack[0];
+}
+
+function operation(a, op, b) {
+    if (op === '+') {
+        return a + b;
+    } else {
+        return a * b;
+    }
+}
+
+function calc2(expr) {
+    const stack = [];
+
+    for (const token of ['(', ...expr, ')']) {
+        let result = 0;
+        if (token === ')') {
+            result = stack.pop();
+            while (true) {
+                let op = stack.pop();
+                if (op === '(') {
+                    break;
+                }
+                result = result * stack.pop();
+            }
+        } else if (typeof token === 'number') {
+            result = token;
+        }
+
+        if (result > 0) {
+            if (stack.length > 0 && stack[stack.length - 1] === '+') {
+                stack.pop(); // remove +
+                result = result + stack.pop();
+            }
+            stack.push(result);
+        } else {
+            stack.push(token);
+        }
+    }
+
+    return stack[0];
+}
 
 /**
  * @param {string} input
@@ -30,7 +93,7 @@ function run(input) {
     const lines = input.split('\n').filter((line) => line.length);
 
     const exprs = [];
-    lines.forEach((line, ix) => {
+    lines.forEach((line) => {
         const expr = line
             .replace(/\s/g, '')
             .split('')
@@ -45,100 +108,15 @@ function run(input) {
     });
 
     for (const expr of exprs) {
-        let result = calc(expr, 0, expr.length - 1);
+        let result = calc1(expr);
         part1 += result;
-    }
-
-    function calc(expr, from, to) {
-        let count = 0;
-        let left;
-        let right;
-        let op;
-        for (let i = to; i >= from; i--) {
-            const token = expr[i];
-            if (token === '(') {
-                count--;
-                if (count === 0) {
-                    right = calc(expr, i + 1, to - 1);
-                }
-            } else if (token === ')') {
-                count++;
-            } else if (count === 0) {
-                if (typeof token === 'number') {
-                    right = token;
-                } else {
-                    op = token;
-                    left = calc(expr, from, i - 1);
-                    break;
-                }
-            }
-        }
-
-        let result;
-        if (op === undefined) {
-            result = right;
-        } else if (op === '*') {
-            result = left * right;
-        } else {
-            result = left + right;
-        }
-        return result;
     }
 
     /////////
 
-    console.time('p2');
     for (const expr of exprs) {
-        const result = calc2(expr, 0, expr.length - 1, '*');
+        const result = calc2(expr);
         part2 += result;
-    }
-    console.timeEnd('p2');
-
-    function calc2(expr, from, to, op) {
-        // console.log(expr.slice(from, to + 1).join(''));
-        if (from === to) {
-            // console.log('const', expr[from]);
-            return expr[from];
-        }
-
-        const values = [];
-        let count = 0;
-        let last = from;
-        for (let i = from; i <= to; i++) {
-            const token = expr[i];
-            if (token === '(') {
-                count++;
-            } else if (token === ')') {
-                count--;
-            }
-            if (count === 0 && (token === op || i === to)) {
-                const nextFrom = last;
-                const nextTo = i === to ? to : i - 1;
-
-                if (i === to && op === '+' && expr[nextFrom] === '(' && expr[nextTo] === ')') {
-                    values.push(calc2(expr, nextFrom + 1, nextTo - 1, '*'));
-                } else {
-                    values.push(calc2(expr, nextFrom, nextTo, '+'));
-                }
-
-                last = i + 1;
-            }
-        }
-
-        let result = 0;
-        if (op === '*') {
-            result = 1;
-            for (const value of values) {
-                result *= value;
-            }
-        } else {
-            for (const value of values) {
-                result += value;
-            }
-        }
-
-        // console.log('===', expr.slice(from, to + 1).join(''), ' -> ', result);
-        return result;
     }
 
     return [part1, part2];
